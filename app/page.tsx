3e8f165b42c05a1c06a3226009026d455f36e07d
Perry Fardella <perry.fardella@live.com>
2025-03-06 09:51:53 +0900

    Add explainer ramble

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
     <div>
-      <header></header>
-      <main></main>
+      <header>The Fucket List</header>
+      <main>
+        <h1>What is the Fucket List?</h1>
+        <p>
+          The Fucket List is a website where you can list, track and share all
+          your stretch goals in life - things you want to do, but they scare
+          you, things you think you may not be able to accomplish.
+        </p>
+        <p>The concept was created by Jesse Itzler</p>
+      </main>
       <footer></footer>
     </div>
   );
