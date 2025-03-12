12cdb5e955c6c91d5aca21d375193950154787b7
Perry Fardella <perry.fardella@live.com>
2025-03-12 14:56:28 +0900

    Add better spacing to the login and signup forms

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,16 @@
 import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight">
+          Log in to your account
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,16 @@
 import SignupForm from "../components/SignupForm";
 
 export default function SignupPage() {
-  return <SignupForm />;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight">
+          Create your account
+        </h2>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <SignupForm />
+      </div>
+    </div>
+  );
 }
