4daa262255a901fb27dec4388692ca4cc524bda2
Perry Fardella <perry.fardella@live.com>
2025-03-12 11:52:33 +0900

    Overhaul Login Form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,5 @@ import LoginForm from "../components/LoginForm";
 import Link from "next/link";
 
 export default function LoginPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <LoginForm />
-      <p className="text-center mt-4">
-        Don&apos;t have an account?{" "}
-        <Link href="/signup" className="text-blue-500 hover:underline">
-          Sign up
-        </Link>
-      </p>
-    </div>
-  );
+  return <LoginForm />;
 }
